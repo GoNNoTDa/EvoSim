9ad6f6c2135c9887d65f14be07dd1bc03f79a152
GoNNoTDa <danign@me.com>
2012-09-26 15:00:20 -0700

    First of all...

--- EvoSimApp/Form1.cs
+++ EvoSimApp/Form1.cs
@@ -6,14 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Universe.Entity;
 
 namespace EvoSimApp
 {
     public partial class Form1 : Form
     {
+        private Master Dios;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dios = new Master();
+            Dios.BigBang();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dios.TimeFreeze();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dios.ActivateTime();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Dios.Dispose();
+            Dios = null;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Dios != null)
+            {
+                label1.Text = Dios.WhatTimeIsIt().ToString();
+            }
+            else
+            {
+                label1.Text = "En espera...";
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Dios.Dispose();
+            Dios = null;
         }
     }
 }
